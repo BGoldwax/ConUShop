8b7a7e6588fe051bf4692c8837bb3d81b69eae20
omarhammoud <omar_hammoud96@hotmail.com>
2018-04-11 14:37:08 -0400

    Added facade file for mongo

--- app/Mongo/Facade.php
+++ app/Mongo/Facade.php
@@ -5,3 +5,9 @@
  * Date: 2018-04-11
  * Time: 2:15 PM
  */
+namespace App\Mongo;
+class Facade extends \Illuminate\Support\Facades\Facade {
+    protected static function getFacadeAccessor() {
+        return 'mongo';
+    }
+}
